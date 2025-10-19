e5e52ec16e25ba6639925152ae0a83eaea47af62
carlosmanoel <carlosmanoelplay12@gmail.com>
2025-10-19 17:23:46 -0300

    atualiza script de monitoramento de logs para filtrar dados sensíveis

--- scripts-linux/monitoramento-logs.sh
+++ scripts-linux/monitoramento-logs.sh
@@ -5,5 +5,12 @@ LOG_DIR="/home/carlosmanoel/Cml/alura/log-processor-project/myapp/logs"
 echo "Verificando logs no diretorio $LOG_DIR"
 
 find $LOG_DIR -name "*.log" -print0 | while IFS= read -r -d '' arquivo; do 
-	echo "ARQUIVO ENCONTRADO $arquivo" 
+	grep "ERROR" $arquivo > "${arquivo}.filtrado"
+       grep "SENSITIVE-DATA" $arquivo >> "${arquivo}.filtrado"
+
+       sed -i 's/User password is .*/User password is REDACTED/g' "${arquivo}.filtrado"
+       sed -i 's/User password reset request with token .*/User password reset request with token REDACTED/g' "${arquivo}.filtrado"
+       sed -i 's/API key leaked: .*/API key leaked: REDACTED/g' "${arquivo}.filtrado"
+       sed -i 's/User credit card last four digits: .*/User credit card last four digits: REDACTED/g' "${arquivo}.filtrado"
+       sed -i 's/User session initiated with token: .*/User session initiated with token: REDACTED/g' "${arquivo}.filtrado"
 done
